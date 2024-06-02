cde308bd5db49fe035aa28ddd9ae1f59f02e0326
massivebird <gdrakemail@gmail.com>
2024-06-01 22:51:08 -0400

    removed: debug output in release mode

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,12 @@ fn main() {
 
     let bf = std::fs::read_to_string(matches.get_one::<String>("src").unwrap()).unwrap();
 
-    let mut computer = Computer::new(5);
+    let mut computer = Computer::new(30000);
 
     let instructions = Inst::from_str(&bf);
 
     computer.execute(&instructions);
 
+    #[cfg(debug_assertions)] // run except in release mode
     dbg!(&computer);
 }
